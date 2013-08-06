37cc40d05ce04fd4ec1c3a2349c75cfe5a1ed7ad
Sam <sjcappella@gmail.com>
2013-08-06 13:58:52 -0400

    Finished stubbing out the ASTNode module.

--- ASTNode.py
+++ ASTNode.py
@@ -10,3 +10,44 @@ class ASTNode:
 class StatementNode(ASTNode):
 	def __init__(self, line_number, token_type, children, statement_type):
 		pass
+
+# Expression node in AST
+class ExpressionNode(ASTNode):
+	def __init__(self, line_number, token_type, children):
+		pass
+
+# Addop node in AST
+class AddopNode(ASTNode):
+	def __init__(self, line_number, symbol):
+		this.line_number = line_number
+		this.symbol = symbol
+
+# Term node in AST
+class Term(ASTNode):
+	pass
+
+# Mulop node in AST
+class MulopNode(ASTNode):
+	def __init__(self, line_number, symbol):
+		this.line_number = line_number
+		this.symbol = symbol
+
+# Factor class in AST (this on may need a little work)
+class FactorNode(ASTNode):
+	pass
+
+# Boolean expression class in AST
+class BooleanExpression(ASTNode):
+	pass
+
+# Relationship Operator class in AST
+class RelopNode(ASTNode):
+	def __init__(self, line_number, symbol):
+		this.line_number = line_number
+		this.symbol = symbol
+
+# Unary Operator class in AST
+class UnaryNode(ASTNode):
+	def __init__(self, line_number, symbol):
+		this.line_number = line_number
+		this.symbol = symbol
