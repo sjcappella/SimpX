b9036d5ab374d883d56f6fd24d82f51b615521a9
Sam <sjcappella@gmail.com>
2013-08-08 16:29:26 -0400

    Structure Change.

---
+++ Src/SimpX.py
@@ -0,0 +1,39 @@
+import sys
+import Parser as Parser
+
+# Function to perform lexical analysis and parsing of source code
+def lexAndParse(source_path):
+   # Open source code file
+   file = open(source_path)
+   
+   # Read in source code line by line
+   source_code = []
+   while 1:
+      line = file.readline()
+      if not line:
+         break
+      else:
+         source_code.append(line)
+
+   # Print out the original source code
+   print("Source Code to execute and analyze:")
+   for x in range(len(source_code)):
+      print(source_code[x])
+
+   # Send source code for lexical analysis and parsing
+   for x in range(len(source_code)):
+      Parser.parse(source_code[x], x+1)
+
+
+def main(argv):
+   
+   if (len(argv)) < 2:
+   	print("No input file given. Quitting.")
+   	exit()
+   else:
+   	lexAndParse(argv[1])
+   
+   
+
+if __name__ == "__main__":
+    main(sys.argv)
